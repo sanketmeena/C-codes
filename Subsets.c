796bf9ecbf646ed5a7eace86031ca9c26563a030
Sanket Meena <84660838+sanketmeena@users.noreply.github.com>
2023-02-13 15:50:07 +0530

    Update Subsets.c

--- Subsets.c
+++ Subsets.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void subset(int *arr,int n,int i,int *arr2,int k)
+void subset(int *arr,int n,int i,int *arr2)
 {
-    if(k==1) arr2[i]=arr[i];
+    //printing arr2 aka subset
     if((n-1)== i)
     {
         for(int j=0; j<n; j++) printf("%d ",arr2[j]);
         printf("\n");
         
-        // for(int j=0; j<n; j++) arr2[j]=0;
         return;
-    }//printing arr2 aka subset
+    }
     
     
-    subset(arr,n,i+1,arr2,0);
-    subset(arr,n,i+1,arr2,1);
-    arr2[i+1]=0;
+    subset(arr,n,i+1,arr2); //Not include
+    arr2[i+1]=arr[i+1];     //Copy to new array
+    subset(arr,n,i+1,arr2); //include
+    arr2[i+1]=0;            //Backtrack
 }
 
 void main()
 {
-    int arr[]={1,2,3};
-    int n=3;
+    int arr[]={1,2,3,4};
+    int n=4;
     int arr2[4];
-    subset(arr,n,-1,arr2,0);
+    subset(arr,n,-1,arr2);
 }
